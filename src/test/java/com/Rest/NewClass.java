3aa961b91e96e73605e1862e0d4cad2f46c95c42
Ranjan273 <thepradhan01@hotmail.com>
2022-02-02 11:57:37 +0530

    New Class Commit

--- src/test/java/com/Rest/NewClass.java
+++ src/test/java/com/Rest/NewClass.java
@@ -1,2 +1,24 @@
-package com.Rest;public class NewClass {
+package com.Rest;
+
+import java.util.ArrayList;
+
+public class NewClass {
+    public NewClass() {
+    }
+
+    public static void main(String[] arg) {
+        int a = 0;
+        ArrayList M = new ArrayList();
+
+        for(int i = 0; i < 100; ++i) {
+            ArrayList subset = new ArrayList();
+            subset.add(a);
+            subset.add(a + 1);
+            M.add(subset);
+            ++a;
+        }
+
+        System.out.println(M);
+    }
+
 }
